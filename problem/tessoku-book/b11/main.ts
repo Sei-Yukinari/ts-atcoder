a51628fe234cabf76419abf405469c6179ec7c83
Sei Yukinari <sei.yukinari@nextremer.com>
2024-03-08 15:04:44 +0900

    tessoku-book/b11

---
+++ problem/tessoku-book/b11/main.ts
@@ -0,0 +1,118 @@
+import * as fs from "fs";
+
+const createInputHandlers = (str: string) => {
+  const inputArray = str.split(/\s/);
+  let currentIndex = 0;
+  let outputBuffer = "";
+
+  const next = () => {
+    return inputArray[currentIndex++];
+  };
+
+  const nextNum = () => {
+    return +next();
+  };
+
+  const nextBigInt = () => {
+    return BigInt(next());
+  };
+
+  const nexts = (length: number) => {
+    const arr = [];
+    for (let i = 0; i < length; ++i) arr[i] = next();
+    return arr;
+  };
+
+  const nextNums = (length: number) => {
+    const arr = [];
+    for (let i = 0; i < length; ++i) arr[i] = nextNum();
+    return arr;
+  };
+
+  const nextBigInts = (length: number) => {
+    const arr = [];
+    for (let i = 0; i < length; ++i) arr[i] = nextBigInt();
+    return arr;
+  };
+
+  const print: {
+    (out: string | number | bigint): void;
+    <T>(out: Array<T>, separator: string): void;
+  } = (out: string | number | bigint | Array<any>, separator?: string) => {
+    if (Array.isArray(out)) {
+      outputBuffer += out.join(separator);
+    } else {
+      outputBuffer += out;
+    }
+  };
+
+  const println: {
+    (out: string | number | bigint): void;
+    <T>(out: Array<T>, separator: string): void;
+  } = (out: string | number | bigint | Array<any>, separator?: string) => {
+    if (Array.isArray(out)) {
+      print(out, separator || "");
+    } else {
+      print(out);
+    }
+    print("\n");
+  };
+
+  function flush() {
+    console.log(outputBuffer);
+  }
+
+  return {
+    next,
+    nextNum,
+    nextBigInt,
+    nexts,
+    nextNums,
+    nextBigInts,
+    print,
+    println,
+    flush,
+  };
+};
+
+export const main = (
+  n: number,
+  a: number[],
+  q: number,
+  x: number[],
+): number[] => {
+  const s = a.sort((a, b) => a - b);
+  const result = x.map((v) => {
+    let count = 0;
+    let left = 0;
+    let right = n;
+    while (left < right) {
+      let mid = Math.floor((left + right) / 2);
+      if (s[mid] < v) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+    count = left;
+    return count;
+  });
+  return result;
+};
+
+const str = fs.readFileSync("/dev/stdin", "utf8");
+if (str !== "") {
+  const input = createInputHandlers(str);
+
+  const n = input.nextNum();
+  const a = input.nextNums(n);
+  const q = input.nextNum();
+  const x: number[] = new Array(q);
+  for (let i = 0; i < q; i++) {
+    x[i] = input.nextNum();
+  }
+
+  const result = main(n, a, q, x);
+  input.print(result.join("\n"));
+  input.flush();
+}
