df0d368db3872b80f2fef78585bd97ae4d640e56
Sei Yukinari <sei.yukinari@nextremer.com>
2024-03-28 09:37:34 +0900

    tessoku-book/b27

--- problem/tessoku-book/b27/main.test.ts
+++ problem/tessoku-book/b27/main.test.ts
@@ -6,5 +6,18 @@ jest.mock("fs", () => ({
 }));
 
 describe("main function", () => {
-  it("", () => {});
+  it("should return the least common multiple of two numbers", () => {
+    const result = main(BigInt(12), BigInt(15));
+    assert.strictEqual(result, BigInt(60));
+  });
+
+  it("should return the number itself when both numbers are the same", () => {
+    const result = main(BigInt(7), BigInt(7));
+    assert.strictEqual(result, BigInt(7));
+  });
+
+  it("should return zero when one of the numbers is zero", () => {
+    const result = main(BigInt(0), BigInt(7));
+    assert.strictEqual(result, BigInt(0));
+  });
 });
